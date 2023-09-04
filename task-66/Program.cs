d24b5fc4155c54111b381f34bbe2f1826a66b404
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-09-04 21:45:12 +0300

    task-66_commit

---
+++ task-66/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите натуральное число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите натуральное число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+
+int Sum(int k, int c)
+{
+     int s = k;
+    if (k == c)
+        return 0;
+    else
+    {
+        k++;
+        s = k + Sum(k, c);
+        return s;
+    }
+}
+
+Console.Write($"{Sum(m-1,n)} "); 
