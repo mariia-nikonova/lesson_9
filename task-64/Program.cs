dd7da5c31da62c3748177878549fa23e9ee234d7
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-09-04 21:07:42 +0300

    tack-64_commit

---
+++ task-64/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите натуральное число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+void number (int k)
+{
+    if (k!=0) 
+    {
+     Console.Write($"{k}, ");   
+     number (k-1);
+    }
+}
+number (n);
